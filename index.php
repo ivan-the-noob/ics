95f69f951376a684179076d62907f0c2265b4454
Ivan <84211523+ivan-the-noob@users.noreply.github.com>
2025-03-14 11:19:24 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="features/admin/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="navbar flex-column shadow-sm p-3 collapse show" id="navbar">
+            <div class="navbar-header d-flex justify-content-between align-items-center w-100">
+                <a class="navbar-brand d-none d-md-block logo-container" href="#">
+                    <img src="assets/logo.png" alt="Logo">
+                </a>
+            </div>
+            <div class="navbar-links">
+                <a href="dashboard.php">
+                    <i class="fa-solid fa-gauge-high"></i>
+                    <span>Dashboard</span>
+                </a>
+                <a href="calendar.php">
+                    <i class="fa-solid fa-calendar-days"></i>
+                    <span>Offerings</span>
+                </a>
+                <a href="pending.php">
+                    <i class="fa-solid fa-clock"></i>
+                    <span>Expenses</span>
+                </a>
+               
+            </div>
+        </div>
+    
+    </div>
+
+    </div>
+    
+</body>
+</html>
