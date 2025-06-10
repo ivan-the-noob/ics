d799cd60384874a13d0d0f1a91dd276732ab69ac
Miles <MilesBriones@cvsu.edu.ph>
2025-06-10 10:30:59 +0800

    a

--- db.php
+++ db.php
@@ -1,9 +1,16 @@
 <?php
     // db.php
+    // $servername = "localhost";
+    // $username = "root"; // your database username
+    // $password = ""; // your database password
+    // $dbname = "ics"; // your database name
+
     $servername = "localhost";
-    $username = "root"; // your database username
-    $password = ""; // your database password
-    $dbname = "ics"; // your database name
+    $username = "u373116035_ics"; // your database username
+    $password = "#sangley2323"; // your database password
+    $dbname = "u373116035_ics"; // your database name
+
+
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
